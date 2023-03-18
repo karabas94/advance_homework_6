d1f0ce20b7984f279facef5ce46a5edf5b36d59c
karabas94 <alkar_94@ukr.net>
2023-03-18 18:06:13 +0200

    -bag fixed

--- pythagor/views.py
+++ pythagor/views.py
@@ -1,24 +1,24 @@
-from django.shortcuts import redirect, render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 from .forms import Pythagor
 
 
 def hypotenuse(request):
-    side_c = request.session.get('side_c', None)
+    side_c = request.GET.get('side_c', '')
     return render(request, 'pythagor/hypotenuse.html', {'side_c': side_c})
 
 
 def index(request):
-    side_a = None
-    side_b = None
     if "ok" in request.GET:
         triangle_form = Pythagor(request.GET)
         if triangle_form.is_valid():
             side_a = triangle_form.cleaned_data['side_a']
             side_b = triangle_form.cleaned_data['side_b']
-            side_c = (side_a ** 2 + side_b ** 2)
-            request.session['side_c'] = side_c
-        return redirect('pythagor:hypotenuse')
+            side_c = (side_a ** 2 + side_b ** 2) ** 0.5
+            redirect_url = reverse('pythagor:hypotenuse') + f'?side_c={side_c}'
+            return HttpResponseRedirect(redirect_url)
     else:
         triangle_form = Pythagor()
-    return render(request, 'pythagor/index.html', {'side_a': side_a, 'side_b': side_b, 'triangle_form': triangle_form})
+    return render(request, 'pythagor/index.html', {'triangle_form': triangle_form})
