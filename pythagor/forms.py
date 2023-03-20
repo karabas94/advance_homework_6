3c556977c5186f9f00855a48a882115fef861ce1
karabas94 <alkar_94@ukr.net>
2023-03-20 17:17:14 +0200

    -created PeopleForm

--- pythagor/forms.py
+++ pythagor/forms.py
@@ -1,5 +1,13 @@
 from django import forms
 
+from .models import People
+
+
+class PeopleForm(forms.ModelForm):
+    class Meta:
+        model = People
+        fields = ['first_name', 'last_name', 'email']
+
 
 class Pythagor(forms.Form):
     side_a = forms.IntegerField(label='lengths of the side "a"', help_text='cm', min_value=1)
